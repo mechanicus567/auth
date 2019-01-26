d1ac1cd2972986ce14d9e89977458d7ce4b08b7c
Anton <developer567@yandex.ru>
2019-01-26 23:30:15 +0300

    getting code by client id

--- src/main/java/by/test/App.java
+++ src/main/java/by/test/App.java
@@ -1,13 +1,79 @@
 package by.test;
 
+import java.awt.*;
+import java.awt.Desktop.Action;
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Pattern;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
+public class App{
+
+    private static final String AUTH_URL = "https://accounts.google.com/o/oauth2/v2/auth";;
+
+    private static final String CLIENT_ID = "1032384937901-ei825nub3iv75q13sa64rhmsfherh0pd.apps.googleusercontent.com";
+    private static final String CLIENT_SECRET = "";
+
+    public static void main( String[] args ) throws IOException, InterruptedException {
+
+        /*if (Desktop.isDesktopSupported()){
+            Desktop desktop = Desktop.getDesktop();
+            if (desktop.isSupported(Action.BROWSE)){
+                desktop.browse(URI.create("http://oauth.com"));
+            }
+        }else {
+
+        }*/
+
+        String redirectUrl = "http://" + InetAddress.getLoopbackAddress().getHostAddress() + ":4546";
+        System.out.println("redirect url: " + redirectUrl);
+
+        ServerSocket serverSocket = new ServerSocket(4546);
+
+
+        String authRequest = String.format("%1$s?response_type=code&scope=openid%%20profile&redirect_uri=%2$s&client_id=%3$s",
+                AUTH_URL,
+                redirectUrl,
+                CLIENT_ID
+                );
+        System.out.println("auth request:" + authRequest);
+
+        Process browser = Runtime.getRuntime().exec("rundll32 url.dll,FileProtocolHandler " + authRequest);
+                //Runtime.getRuntime().exec("rundll32 url.dll,FileProtocolHandler http://oauth.com");
+        Socket socket = serverSocket.accept();
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        String redirectRequest = bufferedReader.readLine();
+        if (redirectRequest != null && redirectRequest.length() > 7){
+            String query = redirectRequest.substring(6);
+            Map<String, String> responseParameters = getQueryMap(query);
+            System.out.println("code : " + responseParameters.get("code"));
+        }
+
+
+
+    }
+
+    public static Map<String, String> getQueryMap(String query)
     {
-        System.out.println( "Hello World!" );
+        String[] params = query.split("&");
+        Map<String, String> map = new HashMap<String, String>();
+        for (String param : params)
+        {
+            String name = param.split("=")[0];
+            String value = param.split("=")[1];
+            map.put(name, value);
+        }
+        return map;
     }
 }
